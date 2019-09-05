07682c497c6034ac783eda021e819133ab46629c
Andrew Taylor <andrew@ataylor.me>
2019-09-05 12:34:32 -0700

    Accept the cookie notice in [vr]

--- .ci/test/visual-regression/onReadyScript.js
+++ .ci/test/visual-regression/onReadyScript.js
@@ -1,4 +1,12 @@
 module.exports = async (page, scenario, vp) => {
+    const cookieButtonSelector = 'button.wp-gdpr-cookie-notice-button';
+    try {
+        await page.waitForSelector(cookieButtonSelector, { timeout: 1500 })
+        await page.click(cookieButtonSelector)
+    } catch (error) {
+        console.log("The cookie notice did not appear.")
+    }
+
     await page.evaluate(_ => {
         window.scrollBy(0, window.innerHeight);
     });
