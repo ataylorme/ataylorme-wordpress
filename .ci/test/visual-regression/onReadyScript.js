ca158ce81ac2ac80b09c1667af545995291d7a03
Andrew Taylor <andrew@ataylor.me>
2019-09-05 13:04:51 -0700

    Use waitForNavigation with networkidle0 instead of a custom function in [vr]

--- .ci/test/visual-regression/onReadyScript.js
+++ .ci/test/visual-regression/onReadyScript.js
@@ -1,36 +1,3 @@
-function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
-    page.on('request', onRequestStarted);
-    page.on('requestfinished', onRequestFinished);
-    page.on('requestfailed', onRequestFinished);
-
-    let inflight = 0;
-    let fulfill;
-    let promise = new Promise(x => fulfill = x);
-    let timeoutId = setTimeout(onTimeoutDone, timeout);
-    return promise;
-
-    function onTimeoutDone() {
-        page.removeListener('request', onRequestStarted);
-        page.removeListener('requestfinished', onRequestFinished);
-        page.removeListener('requestfailed', onRequestFinished);
-        fulfill();
-    }
-
-    function onRequestStarted() {
-        ++inflight;
-        if (inflight > maxInflightRequests)
-            clearTimeout(timeoutId);
-    }
-
-    function onRequestFinished() {
-        if (inflight === 0)
-            return;
-        --inflight;
-        if (inflight === maxInflightRequests)
-            timeoutId = setTimeout(onTimeoutDone, timeout);
-    }
-}
-
 module.exports = async (page, scenario, vp) => {
     const cookieButtonSelector = 'button.wp-gdpr-cookie-notice-button';
     try {
@@ -44,6 +11,6 @@ module.exports = async (page, scenario, vp) => {
         window.scrollBy(0, window.innerHeight);
     });
 
-    await waitForNetworkIdle(page, 500, 0);
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
     // await page.waitFor(1500);
 };
