952847237d44c6843a61cebfeed501aa8de49b14
Andrew Taylor <andrew@ataylor.me>
2019-09-05 12:56:53 -0700

    Add waitForNetworkIdle to [vr] script

--- .ci/test/visual-regression/onReadyScript.js
+++ .ci/test/visual-regression/onReadyScript.js
@@ -1,3 +1,36 @@
+function waitForNetworkIdle(page, timeout, maxInflightRequests = 0) {
+    page.on('request', onRequestStarted);
+    page.on('requestfinished', onRequestFinished);
+    page.on('requestfailed', onRequestFinished);
+
+    let inflight = 0;
+    let fulfill;
+    let promise = new Promise(x => fulfill = x);
+    let timeoutId = setTimeout(onTimeoutDone, timeout);
+    return promise;
+
+    function onTimeoutDone() {
+        page.removeListener('request', onRequestStarted);
+        page.removeListener('requestfinished', onRequestFinished);
+        page.removeListener('requestfailed', onRequestFinished);
+        fulfill();
+    }
+
+    function onRequestStarted() {
+        ++inflight;
+        if (inflight > maxInflightRequests)
+            clearTimeout(timeoutId);
+    }
+
+    function onRequestFinished() {
+        if (inflight === 0)
+            return;
+        --inflight;
+        if (inflight === maxInflightRequests)
+            timeoutId = setTimeout(onTimeoutDone, timeout);
+    }
+}
+
 module.exports = async (page, scenario, vp) => {
     const cookieButtonSelector = 'button.wp-gdpr-cookie-notice-button';
     try {
@@ -10,4 +43,7 @@ module.exports = async (page, scenario, vp) => {
     await page.evaluate(_ => {
         window.scrollBy(0, window.innerHeight);
     });
+
+    await waitForNetworkIdle(page, 500, 0);
+    // await page.waitFor(1500);
 };
